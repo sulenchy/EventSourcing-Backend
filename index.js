ab05cff9f7920a701765fb0841944fc69583c00f
sulenchy <sulaimanabiodun172@gmail.com>
2022-03-21 12:23:53 +0100

    feat(register-route): add register endpoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const dotenv = require("dotenv");
 const database = require("./config/database");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("./model/user");
+
 const express = require("express");
 
 dotenv.config();
@@ -10,5 +15,59 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+
+// Register
+app.post("/register", async(req, res) => {
+// our register logic goes here...
+   try {
+    // Get user input
+    const { firstName, lastName, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(), // sanitize
+      password: encryptedUserPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "5h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Login
+app.post("/login", (req, res) => {
+// our login logic goes here
+});
 
 app.listen(PORT, console.log('server started at port', PORT));
